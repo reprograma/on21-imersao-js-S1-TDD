93e2ab0e0f3da327d2692edf7344c298d0249281
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-18 11:28:17 -0300

    test::ballot_box_with_check: add test generateCombinations

---
+++ exercicios/para-sala/resolucao/generateCombinations.spec.js
@@ -0,0 +1,9 @@
+const generateCombinations = require('../resolucao/generateCombinations')
+
+describe('generateCombinations', () => {
+
+    it('Deve ', () => {
+        const combination = generateCombinations('amor')
+        expect(combination).toStrictEqual(["a", "m", "am", "o", "ao", "mo", "amo", "r", "ar", "mr","amr","or", "aor","mor","amor"])
+    })
+})
