ab038a071364ea8042a7766ec4e552e12594a46c
linefmv <alinemaciel018@gmail.com>
2022-09-18 18:51:17 -0300

    test: WIP TDD challenge

---
+++ exercicios/para-sala/resolucao/calculator/calculator.js
@@ -0,0 +1,31 @@
+// class Calculator {
+//     numero1;
+//     numero2;
+//     // constructor (soma, subtracao, multiplicacao, divisao, fatorial) {
+//     //     this.soma = soma;
+//     //     this.subtracao = subtracao;
+//     //     this.multiplicacao = multiplicacao;
+//     //     this.divisao = divisao; 
+//     //     this.fatorial = fatorial;
+//     // }
+//     // soma(num1, num2) {
+//     //     return num1 + num2
+//     // }
+//     constructor() {}
+
+//     soma(numero1, numero2) {
+//         return numero1 + numero2
+//     }
+// }
+
+class Calculator {
+    constructor() {}
+
+    soma(numero1, numero2) {
+        return numero1 + numero2
+    }
+}
+
+
+
+module.exports = Calculator;
---
+++ exercicios/para-sala/resolucao/calculator/index.test.js
@@ -0,0 +1,20 @@
+const Calculator = require('./calculator')
+
+// describe("calculadora", ()=> {
+//     test("testar função soma", () => {
+//         expect(Calculator.soma(2, 2).toEqual(4))
+//     });
+//     test("testar função de subtração", () => {
+//         expect(calculadora.subtracao(2,3).toEqual(1))
+//     });
+//     test("testar função de multiplicação", () => {
+//         expect(Calculator.multiplicacao(2,3).toEqual(6))
+//     });
+//     test("testar função de divisão", () => {
+//         expect(Calculator.divisao(6, 2).toEqual(3))
+//     });
+//     test("testar função de fatorial", () => {
+//         expect(Calculator.fatorial(6).toEqual(720))
+//     });
+// })
+
