f4e43621d78bc1863256d933123f8b375028a2ea
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-18 11:30:36 -0300

    test::ballot_box_with_check: add test sum

---
+++ exercicios/para-sala/resolucao/sum-TDD.spec.js
@@ -0,0 +1,12 @@
+const sum = require('../resolucao/sum-TDD')
+
+describe("Sum function", () => {
+    it("Deve retornar a soma de 5 e 1", () => {
+
+        expect(sum(5, 1)).toBe(6);
+    });
+
+    it("Deve receber dois números iguais somar e fazer a multiplicação por 3", () => {
+        expect(sum(1, 1)).toEqual(6);
+    });
+});
