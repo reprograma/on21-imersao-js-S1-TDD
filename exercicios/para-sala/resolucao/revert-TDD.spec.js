5e3c581b10eeb53c7975d9863423c70b0ae23207
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-18 11:31:10 -0300

    feat::heavy_check_mark: add revert function

---
+++ exercicios/para-sala/resolucao/revert-TDD.spec.js
@@ -0,0 +1,11 @@
+const revert = require('../resolucao/revert-TDD')
+
+describe("RevertString function", () => {
+    test("Deve receber uma string 'casa' e retornar a string invert 'asac'", () => {
+        expect(revert('casa')).toEqual('asac');
+    });
+
+    test("Deve receber uma string 'javascript' e retornar a string invert 'tpircsavaj'", () => {
+        expect(revert('javascript')).toEqual('tpircsavaj');
+    });
+});
