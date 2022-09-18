d617e216846e6b6758af76dfdaf0738c9f3686a3
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-18 11:22:13 -0300

    feat::heavy_check_mark: add convertCelsiusToFahrenheit function

---
+++ exercicios/para-sala/resolucao/convertCelsiusToFahrenheit.js
@@ -0,0 +1,8 @@
+
+function convertCelsiusToFahrenheit(temperature){
+    let tempConverted = temperature * 9 / 5 + 32;
+     
+    return tempConverted
+}
+
+module.exports = convertCelsiusToFahrenheit
