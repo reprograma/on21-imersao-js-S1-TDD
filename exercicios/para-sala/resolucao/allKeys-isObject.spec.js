50ca22f43f1e88fa28d7dc63f6b75a97ec63f53b
Gabriela Tavares <gabriela.ttavares@gmail.com>
2022-09-18 11:29:28 -0300

    test::ballot_box_with_check: add test allKeys-isObject

---
+++ exercicios/para-sala/resolucao/allKeys-isObject.spec.js
@@ -0,0 +1,18 @@
+const {allKeys, isObject} = require('../resolucao/allKeys-isObject')
+
+
+describe('allKeys', () => {
+
+    it('Deve allKeys', () => {
+        const keys = allKeys(['amor', 'paixao'])
+        expect(keys).toStrictEqual(["0", "1"])
+    })
+})
+
+describe('isObject', () => {
+
+    it('Deve isObject', () => {
+        const object = isObject(['amor', 'paixao'])
+        expect(object).toBeTruthy()
+    })
+})
