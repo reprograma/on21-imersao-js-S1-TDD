85fc1395c12d146adb9683df03a39a2a6013cf8b
mylenaverspeelt <mylena49@hotmail.com>
2022-09-20 18:01:21 -0300

    resposta atv para sala 8

--- exercicios/para-sala/resolucao/Calculadora.js
+++ exercicios/para-sala/resolucao/Calculadora.js
@@ -1,18 +1,34 @@
 class Calculadora {
-     
-  static soma(num1, num2){
-      return num1 + num2
-    }
-  
-    static subtrai(num1, num2){
-      return num1 - num2
-    }
-    static   multiplica(num1, num2){
-      return num1 * num2
-    }
-    static  divide(num1, num2){
-      return num1 / num2
-    }
+  static soma(num1, num2) {
+    return num1 + num2;
   }
 
-  module.exports = {Calculadora}
+  static subtrai(num1, num2) {
+    return num1 - num2;
+  }
+  static multiplica(num1, num2) {
+    return num1 * num2;
+  }
+  static divide(num1, num2) {
+    return num1 / num2;
+  }
+
+  static fatorial(numero) {
+    let num = 1;
+    for (let i = numero; i > 1; i--) {
+      num *= i;
+    }
+    return num;
+  }
+
+  static aoQuadrado(num) {
+    return num * num;
+  }
+
+  static media(num1, num2, num3, num4) {
+    const soma = num1 + num2 + num3 + num4 
+    const media = soma/4
+    return media;
+  }
+}
+module.exports = { Calculadora };
--- exercicios/para-sala/resolucao/Calculadora.test.js
+++ exercicios/para-sala/resolucao/Calculadora.test.js
@@ -13,4 +13,20 @@ describe("calculadora",()=>{
     test('testa função de divisao', ()=>{
         expect(Calculadora.divide(10, 2)).toEqual(5)
     })
+    test('testa a função fatorial', ()=>{
+        expect(Calculadora.fatorial(5)).toEqual(120)
+    })
+    test('testa a função de retornar um número ao quadrado', () => {
+        expect(Calculadora.aoQuadrado(4)).toEqual(16)
+    })
+    test('testa função de calcular média', () => {
+        expect(Calculadora.media(2, 4, 6, 8)).toEqual(5)
+    })
 })
+
+
+
+// Crie uma classe chamada Calculadora;
+// Crie os métodos: soma, subtração, multiplicação, divisão e fatorial;
+// Use a criatividade e crie um último método que utilize pelo menos dois dos métodos criados anteriormente.
+// 9) Escreva um teste (e depois a função) para uma função calcule como um troco (centavos) será dado em moedas. A função deve receber o valor do troco, e um array com as moedas disponíveis.
