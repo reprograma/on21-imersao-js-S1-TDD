2f685d56df458fa682bf326e973db59e0c572dd4
linefmv <alinemaciel018@gmail.com>
2022-09-21 19:29:24 -0300

    test: exercise during the class

---
+++ exercicios/para-sala/resolucao/calculoTroco/index.test.js
@@ -0,0 +1,15 @@
+function receberMoedas(valorTroco, moedasDisponiveis) {
+    let valorTrocoAtual = valorTroco
+    
+    let arrayDeMoedas = []
+  
+    moedasDisponiveis.forEach((moeda) => {
+      while (valorTrocoAtual >= moeda) {
+        valorTrocoAtual -= moeda
+        arrayDeMoedas.push(moeda)
+      }
+    })
+    return arrayDeMoedas
+  }
+  
+  module.exports = receberMoedas
---
+++ exercicios/para-sala/resolucao/calculoTroco/troco.js
@@ -0,0 +1,17 @@
+//  Escreva um teste (e depois a função) para uma função calcule como um troco (centavos) será dado em moedas. A função deve receber o valor do troco, e um array com as moedas disponíveis.
+
+function receberMoedas(valorTroco, moedasDisponiveis) {
+    let valorTrocoAtual = valorTroco
+    
+    let arrayDeMoedas = []
+  
+    moedasDisponiveis.forEach((moeda) => {
+      while (valorTrocoAtual >= moeda) {
+        valorTrocoAtual -= moeda
+        arrayDeMoedas.push(moeda)
+      }
+    })
+    return arrayDeMoedas
+  }
+  
+  module.exports = receberMoedas
