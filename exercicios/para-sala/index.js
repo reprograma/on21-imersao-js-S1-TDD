94d45e1ab6c1f5744f756e7a1b5b08462d2e6c7e
cclabdis <cclabdis@gmail.com>
2022-09-21 16:21:14 -0300

    teste 1 e 2

--- exercicios/para-sala/index.js
+++ exercicios/para-sala/index.js
@@ -1,7 +1,134 @@
+//exercício 1
+// 1) Escreva um teste para função abaixo:
+
 function convertCelsiusToFahrenheit(temperature){
   var tempConverted = temperature * 9 / 5 + 32;
 
   return tempConverted;
 }
 
-module.exports = convertCelsiusToFahrenheit;
+
+// exercício 2
+//2) Escreva ao menos um teste para a função abaixo:
+
+
+function checkEvenNumber(num){
+  let result = '';
+
+  if(isNaN(num)) {
+      result = num + " não é um número";
+  } else {
+      if (num % 2 == 0) {
+          result = num + " é um número par";
+      } else {
+          result = num + " é um número ímpar";
+      }
+  }
+  return result;
+}
+
+//exercício 3
+//3) Escreva ao menos um teste para a função abaixo:
+
+
+function jokenpo(jogada1, jogada2){
+
+  if (jogada1 === jogada2){
+      return 'Empate!';
+  } 
+  if ((jogada1 === 'pedra' && jogada2 === 'papel')
+      || (jogada1 === 'papel' && jogada2 === 'tesoura')
+      || (jogada1 === 'tesoura' && jogada2 === 'pedra')) {
+    return 'Jogador 2 venceu!';
+  }
+
+  return 'Jogador 1 venceu!';
+}
+
+//exercício 4
+//4) Escreva um para a função abaixo, ela retorna as possíveis combinações de caracteres de uma string em um array.
+
+function generateCombinations(word)
+{
+    const chars = [];
+    for (let x = 0, y = 1; x < word.length; x++, y++) 
+    {
+        chars[x] = word.substring(x, y);
+    }
+    const combinations = [];
+    let temp = "";
+    const combinationsLen = Math.pow(2, chars.length);
+
+    for (let i = 0; i < combinationsLen; i++)
+    {
+        temp = "";
+        for (let j = 0; j < chars.length; j++) {
+            if ((i & Math.pow(2,j))){ 
+                temp += chars[j];
+            }
+        }
+        if (temp !== "")
+        {
+            combinations.push(temp);
+        }
+    }
+    
+    return combinations;
+}
+
+//exercício 5
+//5) Escreva um teste para as funções abaixo:
+
+function allKeys(obj) {
+    if (!isObject(obj)) {
+        return [];
+    }
+    
+    const keys = [];
+    for (const key in obj){
+        keys.push(key);
+    } 
+    return keys;
+}
+
+
+function isObject(obj) {
+    const type = typeof obj;
+    return type === 'function' || type === 'object' && !!obj;
+}
+
+//USANDO TDD
+//6) Considere o teste abaixo e escreva a função para que o teste passe.
+// A função soma dois números, caso os números sejam iguais, a soma é triplicada
+
+
+//7) Considere o teste abaixo e escreva a função para que o teste passe.
+
+
+
+//8) Usando TDD:
+//Crie uma classe chamada Calculadora;
+//Crie os métodos: soma, subtração, multiplicação, divisão e fatorial;
+//Use a criatividade e crie um último método que utilize pelo menos dois dos métodos criados anteriormente.
+
+
+//9) Escreva um teste (e depois a função) para uma função calcule como um troco (centavos) será dado em moedas. A função deve receber o valor do troco, e um array com as moedas disponíveis.
+function calculadora( num1, num2){
+  function soma(x, y)
+  {return (x+y);
+  }
+  
+  function subtracao(x, y)
+  {return (x-y);
+  }
+  
+  function multiplicacao(x, y)
+  { return (x*y);
+  }
+  
+  function divisao(x, y)
+  { return (x/y);
+  }
+}
+
+module.exports = {convertCelsiusToFahrenheit, checkEvenNumber, generateCombinations, jokenpo, allKeys, isObject, calculadora};
--- exercicios/para-sala/resolucao/index.test.js
+++ exercicios/para-sala/resolucao/index.test.js
@@ -1,8 +1,80 @@
-const convertCelsiusToFahreinheit = require('../index')
+const {convertCelsiusToFahrenheit, checkEvenNumber, generateCombinations, jokenpo, allKeys, isObject, calculadora} = require('../index.js');
 
+//exercício 1
 describe("Funcao de conversao", () => {
     test("deve converter celsius para fahreinheit", () => {
-      expect (convertCelsiusToFahreinheit(0)).toEqual(32)
+      expect (convertCelsiusToFahrenheit(0)).toEqual(32)
 
   })
 })
+
+
+//exercício 2
+describe("checagem do número impar", () => {
+  test(" deve retornar 1 é um número ímpar", () => {
+    const result = checkEvenNumber("1")
+    expect(result).toEqual("1 é um número ímpar")
+  })
+})
+
+describe("checagem do número par", () => {
+  test(" deve retornar 2 é um número par", () => {
+    const result = checkEvenNumber("2")
+
+    expect(result).toEqual("2 é um número par")
+  })
+})
+
+describe("checagem de não número", () => {
+  test(" deve retornar banana não é um número", () => {
+    const result = checkEvenNumber("banana")
+
+    expect(result).toEqual("banana não é um número")
+  })
+})
+
+//EXERCÍCIO 3 
+
+describe("checagem de empate", () => {
+  test(" deve retornar 2 é um número par", () => {
+    const result = checkEvenNumber("2")
+
+    expect(result).toEqual("2 é um número par")
+  })
+})
+describe("checagem da vitória jogada1", () => {
+  test(" deve retornar 2 é um número par", () => {
+    const result = checkEvenNumber("2")
+
+    expect(result).toEqual("2 é um número par")
+  })
+})
+describe("checagem da vitória jogada 2", () => {
+  test(" deve retornar 2 é um número par", () => {
+    const result = checkEvenNumber("2")
+
+    expect(result).toEqual("2 é um número par")
+  })
+})
+
+describe("Arithmetic functions", () => {
+  test('2 + 3 = 5', () => {
+      expect(soma(2, 3)).toBe(5);
+  });
+
+  test('3 * 4 = 12', () => {
+      expect(mult(3, 4)).toBe(12);
+  });
+
+  test('5 - 6 = -1', () => {
+      expect(sub(5, 6)).toBe(-1);
+  });
+
+  test('8 / 4 = 2', () => {
+      expect(div(8, 4)).toBe(2);
+  });
+
+  test('3!', () => {
+    expect(div(3)).toBe(2);
+  });
+});
