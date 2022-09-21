454fe2ac4a778a902eb59ba3d4ccc9b7f6af557c
amrodrigues <42442977+amrodrigues@users.noreply.github.com>
2022-09-21 16:07:34 -0300

    Atualização do troco
    Usando o parseFloat e o toFixed para fixar o valor em duas casas decimais e acresecentando mais testes

--- exercicios/para-sala/resolucao/calculoTroco.js
+++ exercicios/para-sala/resolucao/calculoTroco.js
@@ -3,12 +3,27 @@ function receberMoedas(valorTroco, moedasDisponiveis) {
     
     let arrayDeMoedas = []
   
-    moedasDisponiveis.forEach((moeda) => {
-      while (valorTrocoAtual >= moeda) {
-        valorTrocoAtual -= moeda
-        arrayDeMoedas.push(moeda)
+    // moedasDisponiveis.forEach((moeda) => {
+    //   console.log('valorTrocoAtual' , valorTrocoAtual)
+       
+    //   while (valorTrocoAtual >= moeda) {
+    //     valorTrocoAtual = parseFloat((valorTrocoAtual - moeda).toFixed(2))
+
+    //     arrayDeMoedas.push(moeda)
+    //   }
+    // })
+  
+    for (let i = 0; valorTrocoAtual > 0; i)
+     {
+      const divisao = parseFloat((valorTrocoAtual / moedasDisponiveis[i]).toFixed(2))
+      if (divisao >= 1) {
+        arrayDeMoedas.push(moedasDisponiveis[i])
+        valorTrocoAtual = parseFloat((valorTrocoAtual - moedasDisponiveis[i]).toFixed(2))
+      } else {
+        i++
       }
-    })
+     }  
+
     return arrayDeMoedas
   }
   
--- exercicios/para-sala/resolucao/troco.test.js
+++ exercicios/para-sala/resolucao/troco.test.js
@@ -9,15 +9,34 @@ describe('Troco', () => {
   })
 
   test('troco para 1,50 retornar um array [1, 0.5]', () => {
-    expect(receberMoedas(1.5, moedasDisponiveis)).toEqual([1, 0.5])
-  });
+    const moedasRetornadasDoTroco = receberMoedas(1.50, moedasDisponiveis)
+    const resultadoEsperado = [1, 0.5]
+    expect(moedasRetornadasDoTroco).toStrictEqual(resultadoEsperado)
+  })
 
   test('troco para 2,00 retornar um array [1, 1]', () => {
-    expect(receberMoedas(2, moedasDisponiveis)).toEqual([1, 1])
+    const moedasRetornadasDoTroco = receberMoedas(2, moedasDisponiveis)
+    const resultadoEsperado =[1, 1]
+    expect(moedasRetornadasDoTroco).toStrictEqual(resultadoEsperado)
   })
-
 
   test('troco para 2,10 retornar um array [1, 1, 0.10]', () => {
-    expect(receberMoedas(2.10, moedasDisponiveis)).toEqual([1, 1, 0.10])
+    const moedasRetornadasDoTroco = receberMoedas(2.10, moedasDisponiveis)
+    const resultadoEsperado =[1, 1, 0.10]
+    expect(moedasRetornadasDoTroco).toStrictEqual(resultadoEsperado)
   })
+
+  test('troco para 2,65 retornar um array [1, 1, 0.50, 0.10 , 0.05]', () => {
+    const moedasRetornadasDoTroco = receberMoedas(2.65, moedasDisponiveis)
+    const resultadoEsperado =[1, 1, 0.50,  0.10 , 0.05]
+    expect(moedasRetornadasDoTroco).toStrictEqual(resultadoEsperado)
+  })
+
+
+  test('troco para 2,75 retornar um array [1, 1, 0.50, 0.25]', () => {
+    const moedasRetornadasDoTroco = receberMoedas(2.75, moedasDisponiveis)
+    const resultadoEsperado =[1, 1, 0.50, 0.25]
+    expect(moedasRetornadasDoTroco).toStrictEqual(resultadoEsperado)
+  })
+
 })
