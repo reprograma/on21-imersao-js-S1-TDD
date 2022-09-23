c9817608c1c84ad6a50a458ff2718aad37d3a506
cclabdis <cclabdis@gmail.com>
2022-09-22 21:03:13 -0300

    code review

---
+++ exercicios/para-casa/entregas/code review/cliente.js
@@ -0,0 +1,8 @@
+const cliente = {
+  nome: 'Joice Flores',
+  saldo: 200,
+  limite: 300,
+  statusLimite: true
+}
+
+module.exports = cliente
---
+++ exercicios/para-casa/entregas/code review/index.test.js
@@ -0,0 +1,57 @@
+const cliente = require('./cliente')
+const Conta = require('./resolucao')
+
+describe('Faz as verificações necessárias em uma conta', () => {
+  const novaConta = new Conta(cliente)
+  let valorSaque = 0
+
+  test('Verifica dados da conta', () => {
+    expect(cliente.nome).toBe('Joice Flores')
+    expect(cliente.saldo).toEqual(200)
+    expect(cliente.limite).toEqual(300)
+  })
+
+  test('Verifica se valor para saque esta disponível', () => {
+    valorSaque = 135
+    let saqueAutorizado = novaConta.verificaSaldo(valorSaque)
+    expect(true).toBe(saqueAutorizado.saqueAutorizadoSaldo)
+
+    valorSaque = 450
+    saqueAutorizado = novaConta.verificaSaldo(valorSaque)
+    expect(true).toBe(saqueAutorizado.saqueAutorizadoLimite)
+
+    valorSaque = 789
+    saqueAutorizado = novaConta.verificaSaldo(valorSaque)
+    expect(false).toBe(saqueAutorizado.saqueAutorizadoLimite)
+  })
+
+  test('Retorna saldo após o saque', () => {
+    valorSaque = 135
+    let valorSaldoAtualizado = novaConta.sacar(valorSaque)
+    expect(valorSaldoAtualizado.saldoAtualizado).toEqual(65)
+
+    valorSaque = 450
+    valorSaldoAtualizado = novaConta.sacar(valorSaque)
+    expect(valorSaldoAtualizado.limiteAtualizado).toEqual(50)
+  })
+
+  test('Ajusta e desativa limite', () => {
+    let novoLimite = 135
+    novaConta.ajustarLimite(novoLimite)
+    expect(novaConta.limite).toEqual(135)
+    expect(novaConta.statusLimite).toEqual(true)
+
+    novoLimite = 850
+    novaConta.ajustarLimite(novoLimite)
+    expect(novaConta.limite).toEqual(850)
+    expect(novaConta.statusLimite).toEqual(true)
+
+    novoLimite = 0
+    novaConta.ajustarLimite(novoLimite)
+    expect(novaConta.statusLimite).toEqual(false)
+
+    novoLimite = -100
+    novaConta.ajustarLimite(novoLimite)
+    expect(novaConta.statusLimite).toEqual(false)
+  })
+})
