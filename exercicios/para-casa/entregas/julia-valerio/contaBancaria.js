52290a6f7b52fe7648f1103baf368a2a58bba410
Julia Valerio <juliav286@gmail.com>
2022-09-30 08:27:03 -0300

    ADD: as funções foram criadas, e passaram nos testes

---
+++ exercicios/para-casa/entregas/julia-valerio/contaBancaria.js
@@ -0,0 +1,62 @@
+/* Exercício - Conta Bancária
+Considere um objeto que represente uma conta bancária, a conta possui o nome do titular, 
+o saldo e o limite da conta. É possível fazer operações de consulta de saldo, saque e depósito. 
+No caso de saque é necessário verificar se existe saldo suficiente para retirada, caso o cliente não tenha saldo suficiente
+para a operação, mas possua limite disponível (e suficiente), o saque poderá ocorrer, nesses casos o saldo do cliente
+ficará negativo após o saque. Além disso, o limite de uma conta pode ser reajustado (para mais e para menos) ou desativado.*/
+
+const contaBancaria = {
+  'nomeTitular': '',
+  'saldoConta': 0,
+  'limite': 0
+}
+
+function getSaldoTotalConta(conta) {
+  let limite = conta.limite != null && conta.limite != undefined ? conta.limite : 0;
+  return conta.saldoConta + limite;
+}
+
+function criarConta(nomeTitular, saldoInicial, limite) {
+  contaBancaria.nomeTitular = nomeTitular;
+  contaBancaria.saldoConta = saldoInicial;
+  contaBancaria.limite = limite;
+  return contaBancaria;
+}
+
+function depositar(conta, valorDeposito) {
+  if (conta.saldoConta == null || conta.saldoConta == undefined) {
+      conta.saldoConta = 0;
+  }
+  conta.saldoConta += valorDeposito;
+  return conta;
+}
+
+function consultarSaldo(conta) {
+  return `O saldo atual de ${conta.nomeTitular} é de R$${conta.saldoConta}`;
+}
+
+function consultarLimite(conta) {
+  return `${conta.nomeTitular}, o limite disponível para a conta é de R$${conta.limite}`;
+}
+
+function sacar(conta, valorSaque) {
+  if (getSaldoTotalConta(conta) >= valorSaque) {
+      conta.saldoConta -= valorSaque;
+      return conta;
+  }
+  return `Não foi possível realizar o saque. Saldo disponível: R$${getSaldoTotalConta(conta)} (Saldo + limite)`;
+}
+
+function alterarLimite(conta, novoLimite) {
+  if (novoLimite == null || novoLimite == undefined) {
+      return "Por favor, informe o novo limite (R$) da conta";
+  }
+  conta.limite = novoLimite;
+  return conta;
+}
+
+function desativarLimite(conta) {
+  return alterarLimite(conta, 0);
+}
+
+module.exports = { criarConta, depositar, consultarSaldo, consultarLimite, sacar, alterarLimite, desativarLimite };
--- exercicios/para-casa/entregas/julia-valerio/resolucao.test.js
+++ exercicios/para-casa/entregas/julia-valerio/resolucao.test.js
@@ -1,52 +1,52 @@
-const { criaConta, deposita, consultaSaldo, consultaLimite, retirar, alteraLimite, desativaLimite } = require('./contaBancaria')
+const { criarConta, depositar, consultarSaldo, consultarLimite, sacar, alterarLimite, desativarLimite } = require('./contaBancaria')
 
 describe("Teste de conta bancária", () => {
-    const conta = {
-        'titular': 'João da Silva',
-        'saldo': 5000,
-        'limite': 10000
+    const contaMock = {
+        'nomeTitular': 'João da Silva',
+        'saldoConta': 500,
+        'limite': 1000
     }
 
     test("Criação de conta", () => {
-        expect(criaConta(conta.titular, conta.saldo, conta.limite)).toEqual(conta);
+        expect(criarConta(contaMock.nomeTitular, contaMock.saldoConta, contaMock.limite)).toEqual(contaMock);
     })
 
     test("Depósito de R$350 em conta com saldo de R$500 = R$850", () => {
-        let valorDeposito = 3500;
-        let saldoPosDeposito = conta.saldo + valorDeposito;
+        let valorDeposito = 350;
+        let saldoPosDeposito = contaMock.saldoConta + valorDeposito;
 
-        expect(deposita(conta, valorDeposito)).toEqual(criaConta(conta.titular, saldoPosDeposito, conta.limite));
+        expect(depositar(contaMock, valorDeposito)).toEqual(criarConta(contaMock.nomeTitular, saldoPosDeposito, contaMock.limite));
     })
 
     test("Consulta saldo de conta", () => {
-        let mensagemRetorno = `O saldo atual de ${conta.titular} é de R$${conta.saldo}`;
-        expect(consultaSaldo(conta)).toEqual(mensagemRetorno);
+        let mensagemRetorno = `O saldo atual de ${contaMock.nomeTitular} é de R$${contaMock.saldoConta}`;
+        expect(consultarSaldo(contaMock)).toEqual(mensagemRetorno);
     })
 
     test("Consulta limite de conta com limite ativo", () => {
-        let mensagemRetorno = `${conta.titular}, o limite disponível para a conta é de R$${conta.limite}`;
-        expect(consultaLimite(conta)).toEqual(mensagemRetorno);
+        let mensagemRetorno = `${contaMock.nomeTitular}, o limite disponível para a conta é de R$${contaMock.limite}`;
+        expect(consultarLimite(contaMock)).toEqual(mensagemRetorno);
     })
 
     test("Saque de valor acima do limite ", () => {
-        let valorSaque = 20000;
-        let limite = conta.limite != null && conta.limite != undefined ? conta.limite : 0;
-        let mensagemRetorno = `Não foi possível realizar o saque. Saldo disponível: R$${conta.saldo + limite} (Saldo + limite)`;
-        expect(retirar(conta, valorSaque)).toEqual(mensagemRetorno);
+        let valorSaque = 2000;
+        let limite = contaMock.limite != null && contaMock.limite != undefined ? contaMock.limite : 0;
+        let mensagemRetorno = `Não foi possível realizar o saque. Saldo disponível: R$${contaMock.saldoConta + limite} (Saldo + limite)`;
+        expect(sacar(contaMock, valorSaque)).toEqual(mensagemRetorno);
     })
 
     test("Saque de valor R$100 ", () => {
-        let valorSaque = 100;
-        let saldoPosSaque = conta.saldo - valorSaque;
-        expect(retirar(conta, valorSaque)).toEqual(criaConta(conta.titular, saldoPosSaque, conta.limite));
+        let valorSaque = 900;
+        let saldoPosSaque = contaMock.saldoConta - valorSaque;
+        expect(sacar(contaMock, valorSaque)).toEqual(criarConta(contaMock.nomeTitular, saldoPosSaque, contaMock.limite));
     })
 
-    test("Altera limite para R$200", () => {
+    test("Alterar limite para R$200", () => {
         let novoLimite = 200;
-        expect(alteraLimite(conta, novoLimite)).toEqual(criaConta(conta.titular, conta.saldo, novoLimite))
+        expect(alterarLimite(contaMock, novoLimite)).toEqual(criarConta(contaMock.nomeTitular, contaMock.saldoConta, novoLimite))
     })
 
-    test("Desativa limite da conta", () => {
-        expect(desativaLimite(conta)).toEqual(criaConta(conta.titular, conta.saldo, 0));
+    test("Desativar limite da conta", () => {
+        expect(desativarLimite(contaMock)).toEqual(criarConta(contaMock.nomeTitular, contaMock.saldoConta, 0));
     })
 })
