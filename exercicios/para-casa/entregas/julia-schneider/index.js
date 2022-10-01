3aa23c4ae0df00a52e93c574166179462f51d01e
Júlia Schneider <julia-msch@hotmail.com>
2022-10-01 19:05:44 -0300

    terminando exercicio para-casa

--- exercicios/para-casa/entregas/julia-schneider/index.js
+++ exercicios/para-casa/entregas/julia-schneider/index.js
@@ -1,19 +1,55 @@
 class ContaBancaria {
-
-  constructor(titular, saldo, limite,) {
-    this.titular = titular
-    this.saldo = saldo
-    this.limite = limite
+  constructor(nome, saldo, limite) {
+      this.nome = nome;
+      this.saldo = saldo;
+      this.limite = limite;
   }
 
-  titular() {
-    return this.titular
+  getSaldo() {
+      return this.saldo
   }
-  saldo() {
-    return this.saldo
+
+  ativarOuDesativarLimite(ativar) {
+      if(ativar === false) {
+          return this.limite = "Limite desativado."
+      }
+      return this.limite
   }
-  limite() {
-    return this.limite
+
+  getLimite() {
+      return this.limite
   }
-};
-module.exports = ContaBancaria;
+
+  setDeposito(valorDoDeposito) {
+      return this.saldo = this.saldo + valorDoDeposito
+  }
+
+  setLimite(aumentarOuDiminuir, valor) {
+      if(this.ativarOuDesativarLimite() == "Limite desativado.") {
+          return "Para editar o valor do limite, você precisa ativá-lo."
+      } if(aumentarOuDiminuir == 'aumentar') {
+          return this.limite += valor
+      } if(aumentarOuDiminuir == 'diminuir') {
+          return this.limite -= valor
+      }
+      return this.limite
+  }
+
+  saque(valorDoSaque) {
+      let limiteTotalDaConta = this.saldo + this.limite
+
+      if(this.saldo < valorDoSaque) {
+          if(limiteTotalDaConta < valorDoSaque) {
+              return "Você não tem saldo nem limite disponível."
+          }
+          limiteTotalDaConta -= valorDoSaque
+
+          return this.saldo -= valorDoSaque
+      }
+      if(this.saldo >= valorDoSaque) {
+          return this.saldo -= valorDoSaque
+      }
+  }
+}
+
+module.exports = ContaBancaria
