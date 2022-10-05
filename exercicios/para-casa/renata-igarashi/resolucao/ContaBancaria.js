f34c8c33b6e3dfef16fa5604480baae4cbcceb97
Renata Igarashi <renata.sayuri@dasa.com.br>
2022-10-05 10:25:33 -0300

    adicionado opcoes de status, aumentar e reduzir limite

--- exercicios/para-casa/renata-igarashi/resolucao/ContaBancaria.js
+++ exercicios/para-casa/renata-igarashi/resolucao/ContaBancaria.js
@@ -4,6 +4,30 @@ class ContaBancaria {
         this.saldo = saldo,
         this.limite = limite
     }
+
+    limiteStatus(status) {
+        if(status === false) {
+            this.limite = false
+        } 
+        else {
+            return this.limite
+        }
+    }
+    aumentarLimite(valor) {
+        if(this.limite === false) {
+            return 'Não é possível aumentar limite, por favor ative o limite e tente novamente'
+        } else {
+            this.limite += valor
+        }
+    }
+    reduzirLimite(valor) {
+        if(this.limite === false) {
+            return 'Não é possível reduzir limite, por favor ative o limite e tente novamente'
+        } else {
+            this.limite -= valor
+        }
+    }    
+
     saque(valorSaque) {
         let saldoTotal = this.saldo + this.limite
 
@@ -16,11 +40,11 @@ class ContaBancaria {
             if(saldoTotal < valorSaque) {
                 return `Saldo insuficiente para saque`
             } else 
-            saldoTotal -= valorSaque
-            return `Valor saque: R$${valorSaque}. Saldo Atual: R$${saldoTotal - this.limite}. Limite: R$${this.saldo + this.limite - valorSaque}`
-           }
-
-         
+            if(this.limite) {
+                saldoTotal -= valorSaque
+                return `Valor saque: R$${valorSaque}. Saldo Atual: R$${saldoTotal - this.limite}. Limite: R$${this.saldo + this.limite - valorSaque}`
+               }   
+            }
     
     }
 }
--- exercicios/para-casa/renata-igarashi/resolucao/ContaBancaria.test.js
+++ exercicios/para-casa/renata-igarashi/resolucao/ContaBancaria.test.js
@@ -1,20 +1,59 @@
 const ContaBancaria = require("./ContaBancaria")
 
 describe('Conta Bancária', () => {
+    beforeEach(() => {
+        return conta = new ContaBancaria("Renata", 1000, 500)
+      })
     test('Deve efetuar saque com sucesso', () => {
-        const contaBancaria = new ContaBancaria('Renata', 300, 1000)
-        const result = contaBancaria.saque(200)
-        expect(result).toEqual('Valor saque: R$200. Saldo Atual: R$100. Limite: R$1000')
+        const result = conta.saque(200)
+        expect(result).toEqual('Valor saque: R$200. Saldo Atual: R$800. Limite: R$500')
     })
 
     test('Deve retornar saldo insuficiente para saque', () => {
-        const contaBancaria = new ContaBancaria('Renata', 300, 50)
-        const result = contaBancaria.saque(500)
+        const result = conta.saque(2000)
         expect(result).toEqual('Saldo insuficiente para saque')
     })
+
     test('Deve retornar saldo negativo ao fazer saque do limite', () => {
-        const contaBancaria = new ContaBancaria('Renata', 100, 50)
-        const result = contaBancaria.saque(140)
-        expect(result).toEqual('Valor saque: R$140. Saldo Atual: R$-40. Limite: R$10')
+        const result = conta.saque(1200)
+        expect(result).toEqual('Valor saque: R$1200. Saldo Atual: R$-200. Limite: R$300')
     })
+
+    test('Deve retornar limite desativado',() => {
+        conta.limiteStatus(false)
+        console.log(conta);
+        expect(conta.limite).toBe(false)
+     })
+
+     test('Deve retornar erro ao tentar sacar valor acima do saldo e sem limite', () => {
+        conta.limiteStatus(false)
+         const result = conta.saque(3100)
+         expect(result).toBe('Saldo insuficiente para saque')
+     })
+     test('Deve retornar erro ao tentar sacar valor acima do saldo e com limite desativado', () => {
+        conta.limiteStatus(false)
+         const result = conta.saque(3100)
+         expect(result).toBe('Saldo insuficiente para saque')
+     })
+     
+     test('Deve retornar aumentar limite com sucesso', () => {
+        conta.aumentarLimite(1000)
+         expect(conta.limite).toBe(1500)
+     })
+
+     test('Deve retornar reduzir limite com sucesso', () => {
+        conta.reduzirLimite(300)
+         expect(conta.limite).toBe(200)
+     })
+     test('Deve retornar erro ao aumentar limite com limite desativado', () => {
+        conta.limiteStatus(false)
+        const result = conta.aumentarLimite(300)
+         expect(result).toBe('Não é possível aumentar limite, por favor ative o limite e tente novamente')
+     })
+
+     test('Deve retornar erro ao reeuzir limite com limite desativado', () => {
+        conta.limiteStatus(false)
+        const result = conta.reduzirLimite(300)
+         expect(result).toBe('Não é possível reduzir limite, por favor ative o limite e tente novamente')
+     })
 })
